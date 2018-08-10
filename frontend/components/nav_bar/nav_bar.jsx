e2ce718e9ec7c54fc5a66945f745aaf372fe619c
gty3310 <guoty3310@gmail.com>
2018-08-10 10:43:38 -0700

    setup folder for styling and scss reset file

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
+
 const NavBar = ({currentUser, logout, openModal}) => {
   const sessionLinks = () => (
     <nav className="login-signup">
-      <button onClick={() => openModal('login')}>Login</button>
-      &nbsp;or&nbsp;
-      <button onClick={() => openModal('signup')}>Signup</button>
+      <button className="white-button" onClick={() => openModal('login')}>
+        LOG IN
+      </button>
+      <button className="orange-button" onClick={() => openModal('signup')}>
+        SIGN UP
+      </button>
     </nav>
   );
   const personalGreeting = () => (
