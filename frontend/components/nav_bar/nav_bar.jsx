6bd769028f1db44d3a89efd84449b389cedc5b5c
gty3310 <guoty3310@gmail.com>
2018-08-10 10:54:06 -0700

    change NavBar component from functional component to class component

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -1,27 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-
-
-const NavBar = ({currentUser, logout, openModal}) => {
-  const sessionLinks = () => (
-    <nav className="login-signup">
-      <button className="white-button" onClick={() => openModal('login')}>
-        LOG IN
-      </button>
-      <button className="orange-button" onClick={() => openModal('signup')}>
-        SIGN UP
-      </button>
-    </nav>
-  );
-  const personalGreeting = () => (
-    <hgroup className="header-group">
-      <h2 className="header-name">Hi, {currentUser.username}!</h2>
-      <button className="header-button" onClick={logout}>Log Out</button>
-    </hgroup>
-  );
-
-  return currentUser ? personalGreeting() : sessionLinks();
-};
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render(){
+    const sessionLinks = () => (
+      <nav className="login-signup">
+        <button className="white-button" onClick={() =>
+            this.props.openModal('login')}>
+          LOG IN
+        </button>
+        <button className="orange-button" onClick={() =>
+            this.props.openModal('signup')}>
+          SIGN UP
+        </button>
+      </nav>
+    );
+    const personalGreeting = () => (
+      <hgroup className="header-group">
+        <h2 className="header-name">Hi, {this.props.currentUser.username}!</h2>
+        <button className="header-button" onClick={this.props.logout}>
+          Log Out</button>
+      </hgroup>
+    );
+    return this.props.currentUser ? personalGreeting() : sessionLinks();
+  }
+}
 
 export default NavBar;
