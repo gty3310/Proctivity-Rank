50c0dddc6c57b7ec5c4c10dda862ef862421f49e
gty3310 <guoty3310@gmail.com>
2018-08-13 10:02:40 -0700

    add sign up error message

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,11 +1,11 @@
 class Api::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
-    if @user.save!
+    if @user.save
       login(@user)
       render :show
     else
-      render json: @user.errors.full_messages, status: 422
+      render json: ["Invalid username/password/email"], status: 422
     end
   end
 
